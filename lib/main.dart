f8569f10e331c0a96a86c1fe75594b98ea27a834
abdalrhmnnassan <abdalrhmnnassan1000@gmail.com>
2022-08-27 16:29:37 +0300

    --init app

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:workmanager/workmanager.dart';
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Workmanager().initialize(callBackDispatcher);
+  Workmanager().registerPeriodicTask("bbb", "ccc" , frequency: const Duration(minutes: 9));
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+@override
+  void initState() {
+  var android =  const AndroidInitializationSettings('@mipmap/ic_launcher');
+  var iOS =  const IOSInitializationSettings();
+  var initSettings =  InitializationSettings(android: android, iOS: iOS);
+  flutterLocalNotificationsPlugin.initialize(initSettings,
+      onSelectNotification: (p){
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: const Text('Notification'),
+            content: Text('$p'),
+          ),
+        );
+      });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+showNotification() async {
+  var android = const AndroidNotificationDetails(
+      'channel id', 'channel NAME', channelDescription: 'CHANNEL DESCRIPTION',
+      priority: Priority.high,importance: Importance.max
+  );
+  var iOS = const IOSNotificationDetails();
+  var platform = NotificationDetails(android: android,iOS:  iOS);
+  await flutterLocalNotificationsPlugin.show(
+      0, 'New Video is out', 'Flutter Local Notification', platform,
+      payload: 'Nitish Kumar Singh is part time Youtuber');
+}
+void callBackDispatcher() {
+  Workmanager().executeTask((taskName, inputData) {
+    print(taskName);
+    print(inputData);
+    showNotification();
+    return Future.value(true);
+  });
+}
